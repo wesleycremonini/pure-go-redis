6d0ed8f4fc81b09371de6bfc35b2554051cc4b92
wesleycremonini <wesleycremonini0@gmail.com>
2023-06-02 20:17:15 -0300

    feat: listen for connections

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wesleycremonini/pure-go-redis
+
+go 1.20
--- main.go
+++ main.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"log"
+	"net"
+	"sync"
+)
+
+// cache stores the keys
+var cache sync.Map
 
 func main() {
-	
+	// starts a new listener
+	newtwork, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Listening on tcp://0.0.0.0:5000")
+
+	// waits for the next connection on the network
+	conn, err := newtwork.Accept()
+	log.Println("There is a new connection: ", conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// keep the main goroutine alive (only for testing purposes)
+	select{}
 }
